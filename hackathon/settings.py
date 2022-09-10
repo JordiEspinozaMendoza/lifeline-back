a7b58884c5533f1192dc0cb2bd67cd93ff366b89
IsaacRFx <edwin.rodriguez193@tectijuana.edu.mx>
2022-09-09 22:52:55 -0700

    Requirements and models

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
+    'rest_framework',
     'django.contrib.postgres',
     'corsheaders',
     'api',
